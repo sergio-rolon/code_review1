5e767e6a76d14bd1fa19858fcf091d3cfae75ca7
sergio-rolon <rolonsergio30@gmail.com>
2024-09-26 12:20:13 -0600

    Se agregan algunas correcciones, pendiente terminar

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,25 @@
-const baseEndpoint = 'https://api.github.com';
+const baseEndpoint = "https://api.github.com";
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
-
-function displayUser(username) {
-  $n.textContent = 'cargando...';
+// Se corrigieron los querySelectors ya que estaban con identificadores erróneos
+const $n = document.querySelector("name");
+const $b = document.querySelector("blog");
+const $l = document.querySelector("location");
+// esta función tiene que llamar a otras para recuperar los datos, por tiempo no se termino de realizar
+// tratando de usar la función async y await
+async function displayUser(username) {
+  $n.textContent = "cargando...";
   const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  const data = async () => {
+    await response.json();
+    return movies;
+  };
 }
 
 function handleError(err) {
-  console.log('OH NO!');
+  console.log("OH NO!");
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  n.textContent = `Algo salió mal: ${err}`;
 }
 
-displayUser('stolinski').catch(handleError);
+//Este llamado de función solo debe pasar parámetro y no devolver una promesa, idealmente
+displayUser("stolinski");
